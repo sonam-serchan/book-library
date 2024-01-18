eb27fd246fd94d61ad6f7327692b97996e5d2829
Sonam Serchan <sonam.serchan@objective.com>
2024-01-18 19:04:10 +1100

    login and reset-password forms added

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,10 +1,50 @@
+import { Button, Form } from "react-bootstrap";
 import BaseLayout from "../../components/layout/BaseLayout";
+import CustomInput from "../../components/customInput/customInput";
+import { useState } from "react";
+import { toast } from "react-toastify";
+
+const inputs = [
+  { name: "email", label: "Email", placeholder: "abc@abc.com", type: "email", required: true },
+  { name: "password", label: "Password", placeholder: "********", type: "password", required: true, minLength: 6 },
+]
 
 const Login = () => {
+  const [formData, setFormData] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value }  = e.target;
+
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // validate inputs
+    // Todo: 
+
+    toast("Logged in!");
+  }
+
   return (
     <>
       <BaseLayout>
-        Login component
+        <div className="p-3 border shadow rounded admin-form">
+          <h1>Login</h1>
+          <Form onSubmit={handleSubmit}>
+            {inputs.map(input => (
+              <CustomInput key={input.name} label={input.label} {...input} onChange={handleChange} />
+            ))}
+
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </div>
       </BaseLayout>
     </>
   )
--- src/pages/auth/ResetPassword.jsx
+++ src/pages/auth/ResetPassword.jsx
@@ -1,10 +1,49 @@
+import { Button, Form } from "react-bootstrap";
 import BaseLayout from "../../components/layout/BaseLayout";
+import CustomInput from "../../components/customInput/customInput";
+import { useState } from "react";
+import { toast } from "react-toastify";
+
+const inputs = [
+  { name: "email", label: "Email", placeholder: "abc@abc.com", type: "email", required: true },
+]
 
 const ResetPassword = () => {
+  const [formData, setFormData] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value }  = e.target;
+
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // validate inputs
+    // Todo: 
+
+    toast("Password reset!");
+  }
+
   return (
     <>
       <BaseLayout>
-        ResetPassword component
+        <div className="p-3 border shadow rounded admin-form">
+          <h1>Password Reset</h1>
+          <Form onSubmit={handleSubmit}>
+            {inputs.map(input => (
+              <CustomInput key={input.name} label={input.label} {...input} onChange={handleChange} />
+            ))}
+
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </div>
       </BaseLayout>
     </>
   )
