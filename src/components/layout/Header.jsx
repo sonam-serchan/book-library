90d681c59d12c438895b40be2493e57c78a3a1bf
Sonam Serchan <sonam.serchan@objective.com>
2024-01-23 21:05:54 +1100

    Logout

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,24 @@
+import { signOut } from "firebase/auth";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { auth } from "../../firebase-config";
+import { useDispatch } from "react-redux";
+import { setUserInfo } from "../../redux/auth/authSlice";
+import { toast } from "react-toastify";
 
 const Header = () => {
+  const { userInfo } = useSelector(state => state.auth);
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    signOut(auth).then(() => {
+      dispatch(setUserInfo({}));
+    }).catch((error) => {
+      toast.error(error.message)
+    });
+  }
+
   return (
     <Navbar expand="lg" bg="dark" variant="dark">
       <Container>
@@ -11,7 +28,11 @@ const Header = () => {
           <Nav className="ms-auto">
             <Link to="/dashboard" className="nav-link">Dashboard</Link>
             <Link to="/admin-signup" className="nav-link">Signup</Link>
-            <Link to="/login" className="nav-link">Login</Link>
+            {userInfo.uid ? (
+              <Link to="#" onClick={handleLogout} className="nav-link">Logout</Link>
+            ): (
+              <Link to="/login" className="nav-link">Login</Link>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
